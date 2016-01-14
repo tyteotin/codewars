25c3edf2a888a2a40a2cc37100d9e00951e81982
tyteotin <tyteotin@users.noreply.github.com>
2016-01-13 22:52:32 -0800

    Update rot13_a.py

--- rot13_a.py
+++ rot13_a.py
@@ -1,4 +1,4 @@
-""
+"""
 How can you tell an extrovert from an introvert at NSA? Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf.
 
 I found this joke on USENET, but the punchline is scrambled. Maybe you can decipher it? According to Wikipedia, ROT13 
@@ -8,7 +8,8 @@ Hint: For this task you're only supposed to substitue characters. Not spaces, pu
 
 Test.expect(rot13("EBG13 rknzcyr.") == "ROT13 example.");
 Test.expect(rot13("This is my first ROT13 excercise!") == "Guvf vf zl svefg EBG13 rkprepvfr!")
-""
+"""
+
 def rot13_a(message):
     msg = ""
     for i in message:
