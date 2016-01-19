a3d1974b104800b049afd5366ccdfbbf6aed054e
tyteotin <tyteotin@users.noreply.github.com>
2016-01-19 09:32:51 -0800

    Create order.py

---
+++ 6_kyu/order.py
@@ -0,0 +1,23 @@
+"""
+Your task is to sort a given string. Each word in the String will contain a single number. This number is the position the word 
+should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input String is empty, return an empty String. The words in the input String will only contain valid consecutive numbers.
+
+For an input: "is2 Thi1s T4est 3a" the function should return "Thi1s is2 3a T4est"
+"""
+
+import copy
+def order(sentence):
+    if(sentence == ""):
+        return ""
+    sentence = sentence.split()
+    original = copy.deepcopy(sentence)
+    s = [loc for i in sentence for loc in i if loc.isdigit()]
+    count = 0
+    for i in s:
+        sentence[int(i)-1] = original[count]
+        count = count + 1
+    return " ".join(sentence)
